de9358a29ab0c22b3d1714e08640f9bb7c4257e2
afiaFarjana1996 <afia.farjana@smoothstack.com>
2020-01-28 14:56:14 -0500

    added unit test for get method

--- handlers/getCustomerData.js
+++ handlers/getCustomerData.js
@@ -10,20 +10,40 @@ function response(statusCode, response){
     body: JSON.stringify(response)
   }
 }
-module.exports.getCustomer = (event,context,callback) => {
+exports.getCustomer = (event,callback) => {
 
    const params = {
-       TableName: process.env.TABLE_NAME,
+       TableName: "Customer",
        Key: {
            customerId:  event.pathParameters.customerId
        }
    }
 
-   return documentClient.get(params).promise()
+    return documentClient.get(params).promise()
    .then((res) => {
-    if(res.Item) callback(null, response(200, res.Item));
-    else callback(null, response(404, { error: 'Customer not found' }));
+    if(res.Item) {
+      console.log("hit the inside .then function. ");
+      return response(200, res.Item);
+    }
+    else {
+      console.log("inside else");
+     return response(404, { error: 'Customer not found' });
+    }
    })
-   .catch((err) => callback(null, response(err.statusCode, err)));
+   .catch((err) =>{ return response(err.statusCode, err) }); 
+
+  // return documentClient.get(params).promise()
+  //  .then((res) => {
+  //   if(res.Item) {
+  //     console.log("hit the inside .then function. ");
+  //     callback(null, response(200, res.Item));
+  //   }
+  //   else {
+  //     console.log("inside else");
+  //     callback(null, response(404, { error: 'Customer not found' }));
+  //   }
+  //  })
+  //  .catch((err) => callback(null, response(err.statusCode, err)));
+
    
 }
--- test/unitTest.js
+++ test/unitTest.js
@@ -1,11 +1,11 @@
 const chai  = require('chai');
 const expect = chai.expect;
-const getCustomerModule = require('../handlers/getCustomerData');
+const getCustomerClass = require('../handlers/getCustomerData');
 const table_name = process.env.TABLE_NAME ;
  
 describe('getRecord', () => {
  
-    it('getRecord with valid response', () => {
+    it('getRecord with valid response', (done) => {
         const event = {
             body: '',
             headers: {},
@@ -25,15 +25,19 @@ describe('getRecord', () => {
             "lastName":"farjana",
             "customerId":"d93e3329-994b-434c-a134-d08ab82f137a"
         }
-
-        getCustomerModule.getCustomer(event,null)
-        .then( res =>{
-            expect(res.statusCode).to.equal(200);
-            expect(JSON.parse(res.body)).to.deep.equal(actualResponseBody);
-            console.log("response: "+res.statusCode+" "+res.body);
-        })
-        .catch(err => console.log(err));
+        console.log("before unit test function");
         
-    });
+      getCustomerClass.getCustomer(event,done)
+        .then( res =>{
+            console.log("response: "+res.statusCode+" "+res.body);
+            expect(res.statusCode).to.equal(200);
+            //expect(JSON.parse(res.body)).to.deep.equal(actualResponseBody);
+            console.log("response: a"+res.statusCode+" "+res.body);
+        })
+        .catch(err => console.log("Error is: "+err))
+        .finally(done)
+        ;
+        
+     });
  
 });
